34da1f2bc3af523cfd0517662cdc8a2052277754
Kelly McDonald <kellymcdonald@Admins-MacBook-Air.local>
2022-11-05 14:09:55 -0700

    user auth and fitness model works

--- controllers/tenndr.js
+++ controllers/tenndr.js
@@ -1,61 +1,94 @@
 const express = require("express")
-const tenndrRouter = express.Router()
+const tenndrRouter = require("express").Router()
+const session = require("express-session")
 const Tenndr = require("../models/tenndr")
+const User = require("../models/user")
 
 // index
 tenndrRouter.get("/", (req, res) => {
-    Tenndr.find({}, (err, foundTenndr) => {
-    res.render("index_workouts.ejs", {
-        tenndr: foundTenndr})
-    })
-})
+    if(req.session.currentUser) {
+    User.findById(req.session.currentUser,(err, foundUser) => {
+    res.render("tenndr/dashboard.ejs", {
+        tenndr: foundUser.tenndrs,
+        currentUser: foundUser,
+    });
+    });
+    } else {
+        res.render("tenndr/index.ejs", {
+            currentUser: req.session.currentUser,
+            });
+        } 
+    });
+
 
 // new
 tenndrRouter.get("/new", (req, res) => {
-    res.render("new_workout.ejs")
+    res.render("tenndr/new_workout.ejs", {
+        currentUser: req.session.currentUser,
+    })
 })
 //delete
 tenndrRouter.delete("/:id", (req, res) => {
-    Tenndr.findByIdAndRemove(req.params.id, (err, deletedWorkout) => {
-        res.redirect("/tenndr")
-    })
-})
+    User.findById(req.session.currentUser, (err, foundUser) => {
+        foundUser.tenndrs.id(req.params.id).remove();
+        foundUser.save((err, data) => {
+            res.redirect("/tenndr");
+        });
+    });
+});
 
 
 //update
 tenndrRouter.put("/:id", (req, res) => {
+    User.findById(req.session.currentUser, (err, foundUser) => {
     Tenndr.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        {new: true},
+        req.params.id, 
+        req.body, 
+        {new: true}, 
         (err, updatedWorkout) => {
-            res.redirect(`/tenndr/${req.params.id}`)
-        }
+        res.redirect("/tenndr")
+    }  
     )
+})
 })
 
 //create
 tenndrRouter.post("/", (req, res) => {
-    Tenndr.create(req.body, (err, createdWorkout) => {
-        res.redirect("/tenndr")
-    
-    })
+    User.findById(req.session.currentUser, (err, foundUser) => {
+        Tenndr.create(req.body, (err, createdWorkout) => {
+            foundUser.tenndrs.push(createdWorkout)
+            foundUser.save((err, data) => {
+                res.redirect("/tenndr")
+            })
+        })
+
+    })  
 })
+
 
 //edit
 tenndrRouter.get("/:id/edit", (req, res) => {
-    Tenndr.findById(req.params.id, (err, foundWorkout) => {
-        res.render("edit_workout.ejs", {
-            tenndr: foundWorkout
+    User.findById(req.session.currentUser, (err, foundUser) => {
+        Tenndr.findById(req.params.id, (err, foundWorkout) => {
+            res.render("tenndr/edit_workout.ejs", {
+                tenndr: foundWorkout,
+                currentUser: foundUser,
+            })
         })
     })
 })
 
+
 //show
 tenndrRouter.get("/:id", (req, res) => {
-    Tenndr.findById(req.params.id, (err, foundWorkout) => {
-    res.render("show_workout.ejs", {tenndr: foundWorkout})
-})
+    User.findById(req.session.currentUser, (err, foundUser) => {
+        Tenndr.findById(req.params.id, (err, foundWorkout) => {
+            res.render("tenndr/show_workout.ejs", {
+                tenndr: foundWorkout,
+                currentUser: foundUser,
+            })
+        })
+    })
 })
 
 module.exports = tenndrRouter;
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
+const session = require("express-session")
 const methodOverride = require("method-override")
 const tenndrController = require("./controllers/tenndr")
+const userController = require("./controllers/users")
+const sessionController = require("./controllers/sessions")
+
+
 
 require("dotenv").config()
 const PORT = process.env.PORT
@@ -20,9 +25,29 @@ db.on("disconnected", () => console.log("mongo disconnected"))
 
 //middleware
 app.use(express.urlencoded({extended: true}))
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+)
 app.use(methodOverride("_method"))
 app.use("/tenndr", tenndrController)
+app.use("/users", userController)
+app.use("/sessions", sessionController)
 app.use(express.static("public"))
+
+//routes / controllers
+
+// const tenndrController = require("./controllers/tenndr");
+// app.use("/tenndr", tenndrController);
+
+// const userController = require("./controllers/users");
+// app.use("/users", userController);
+
+// const sessionController = require("./controllers/sessions");
+// app.use("/sessions", sessionController);
 
 
 app.listen(PORT, () => {
