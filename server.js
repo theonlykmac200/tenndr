3e752705f4cf064eb58bd2e2390ea555b592f2e1
Kelly McDonald <kellymcdonald@Admins-MacBook-Air.local>
2022-11-08 19:49:18 -0800

    port change

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const sessionController = require("./controllers/sessions")
 
 
 require("dotenv").config()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000;
 
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
